85099552afcc4ce774c2ee14fe6803269dc6501d
tocausan <tocausan@gmail.com>
2023-01-16 12:29:50 +0100

    cd error correction

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import styles from "@/styles/components/Footer.module.css";
+import Link from "next/link";
 import { Codepen, Instagram, Mail } from "react-feather";
 import { FaGithub, FaLinkedin, FaBehance, FaPinterest } from "react-icons/fa";
 
@@ -8,34 +9,34 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.media}>
-        <a href='http://bit.ly/3XwVLgF' title='tocausan@Github' target='_blank' rel='noreferrer'>
+        <Link href='http://bit.ly/3XwVLgF' title='tocausan@Github' target='_blank' rel='noreferrer'>
           <i className='fab fa-github'></i>
           <FaGithub color='black' size={20} />
-        </a>
-        <a href='http://bit.ly/3X6WKUJ' title='tocausan@Codepen' target='_blank' rel='noreferrer'>
+        </Link>
+        <Link href='http://bit.ly/3X6WKUJ' title='tocausan@Codepen' target='_blank' rel='noreferrer'>
           <i className='fab fa-codepen'></i>
           <Codepen color='black' size={20} />
-        </a>
-        <a href='https://bit.ly/3CNyi2S' title='tocausan@Linkedin' target='_blank' rel='noreferrer'>
+        </Link>
+        <Link href='https://bit.ly/3CNyi2S' title='tocausan@Linkedin' target='_blank' rel='noreferrer'>
           <i className='fab fa-linkedin'></i>
           <FaLinkedin color='black' size={20} />
-        </a>
-        <a href='http://bit.ly/3H3JICa' title='tocausan@Pinterest' target='_blank' rel='noreferrer'>
+        </Link>
+        <Link href='http://bit.ly/3H3JICa' title='tocausan@Pinterest' target='_blank' rel='noreferrer'>
           <i className='fab fa-pinterest'></i>
           <FaPinterest color='black' size={20} />
-        </a>
-        <a href='http://bit.ly/3CKsiHQ' title='tocausan@Instagram' target='_blank' rel='noreferrer'>
+        </Link>
+        <Link href='http://bit.ly/3CKsiHQ' title='tocausan@Instagram' target='_blank' rel='noreferrer'>
           <i className='fab fa-instagram'></i>
           <Instagram color='black' size={20} />
-        </a>
-        <a href='http://bit.ly/3IIexNV' title='tocausan@Behance' target='_blank' rel='noreferrer'>
+        </Link>
+        <Link href='http://bit.ly/3IIexNV' title='tocausan@Behance' target='_blank' rel='noreferrer'>
           <i className='fab fa-behance'></i>
           <FaBehance color='black' size={20} />
-        </a>
-        <a href='mailto: hello@tcsn.io' title='hello@tcsn.io' target='_blank' rel='noreferrer'>
+        </Link>
+        <Link href='mailto: hello@tcsn.io' title='hello@tcsn.io' target='_blank' rel='noreferrer'>
           <i className='far fa-envelope'></i>
           <Mail color='black' size={20} />
-        </a>
+        </Link>
       </div>
       <div className={styles.copyright}>
         <p>tocausan | {new Date().getFullYear()}</p>
