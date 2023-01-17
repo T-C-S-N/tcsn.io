5025a8c5c873944ba37e1ed19cec10642a597ec4
tocausan <tocausan@gmail.com>
2023-01-17 14:45:02 +0100

    SEO

--- src/components/layout/SEO.tsx
+++ src/components/layout/SEO.tsx
@@ -1,7 +1,6 @@
 /** @format */
 
 import Head from "next/head";
-import config from "@/lib/config";
 
 export default function SEO({ description, title, siteTitle }: { description: string, title: string, siteTitle: string }) {
   return (
@@ -20,10 +19,6 @@ export default function SEO({ description, title, siteTitle }: { description: st
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
       <meta property='og:site_name' content={siteTitle} />
-      <meta property='twitter:card' content='summary' />
-      <meta property='twitter:creator' content={config.social.github.id} />
-      <meta property='twitter:title' content={title} />
-      <meta property='twitter:description' content={description} />
     </Head>
   );
 }
