72c1714652966182e3d7d629440bc8fe19321fd5
tocausan <tocausan@gmail.com>
2023-01-20 09:31:37 +0100

    routes

--- next.config.js
+++ next.config.js
@@ -10,6 +10,20 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  async redirects() {
+    return [
+      {
+        source: "/from-facebook",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/from-linkedin",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
