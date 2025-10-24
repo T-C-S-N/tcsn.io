aa70912277f0a0bdbb1460ee99d97fe27d0d308b
toca <tomas@tcsn.io>
2025-10-24 16:56:04 +0200

    app colors

--- src/components/AppColors.vue
+++ src/components/AppColors.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex flex-row">
     <div v-for="color in colors" :key="color.name" class="flex items-center">
-      <div :class="`w-[60px] h-[40px] bg-${color.class}`"></div>
+      <div
+        :class="`bg-${color.class}`"
+        :style="`width: ${width}px; height: ${height}px;`"
+      ></div>
     </div>
   </div>
 </template>
@@ -9,13 +12,24 @@
 <script setup>
 import { ref } from 'vue'
 
+defineProps({
+  width: {
+    type: Number,
+    default: 60
+  },
+  height: {
+    type: Number,
+    default: 40
+  }
+})
+
 const colors = ref([
   { name: 'primary', class: 'primary' },
   { name: 'secondary', class: 'secondary' },
   { name: 'tertiary', class: 'tertiary' },
   { name: 'quaternary', class: 'quaternary' },
   { name: 'accent', class: 'accent' },
-  { name: 'background', class: 'background' },
+  { name: 'background', class: 'background' }
   //{ name: 'text', class: 'text' },
   //{ name: 'success', class: 'success' },
   //{ name: 'error', class: 'error' },
